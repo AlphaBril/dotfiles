4b636dd9421a4d29c2e9b0d70a6cc9bdd6c671b3
alphabril <florian.marie.doucet@gmail.com>
2023-12-19 17:50:50 +0100

    fix hardware

--- nix-config/hardware.nix
+++ nix-config/hardware.nix
@@ -5,6 +5,35 @@ rec {
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/2EF9-904A";
+      fsType = "vfat";
+    };
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/5132f872-a526-418e-ae41-00aef53741d3";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/e468c2ac-80f1-4d57-baa9-eea394ecced1"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader = {
     timeout = 1;
@@ -16,35 +45,6 @@ rec {
   boot.loader.efi.canTouchEfiVariables = true;
   boot.plymouth.enable = true;
 
-  fileSystems = 
-    let 
-      bt = d: {
-        device = "/dev/disk/by-uuid/ea00659b-c3ec-4972-be77-9193baf408be";
-        fsType = "btrfs";
-        options = [ "subvol=@${d}" "compress=zstd" ];
-      }; in {
-
-    "/" = bt "";
-    "/home" = bt "home";
-    "/nix" = bt "nix";
-    "/var" = bt "var";
-
-    "/boot" = {
-       device = "/dev/disk/by-uuid/A4A5-D3C9";
-       fsType = "vfat";
-    };
-  };
-
-  swapDevices = [
-    {
-      device = "/swap";
-      size = 32 * 1024;
-    }
-  ];
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
@@ -54,39 +54,6 @@ rec {
       libvdpau-va-gl
     ];
   };
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  services.tlp = {
-    enable = true;
-    settings = {
-      START_CHARGE_THRESH_BAT0=80;
-      STOP_CHARGE_THRESH_BAT0=100;
-
-      # https://linrunner.de/tlp/settings/processor.html#cpu-scaling-governor-on-ac-bat
-
-      # Energy perf: power, performance
-      # Scaling: powersave, performance
-
-      # Battery
-      CPU_SCALING_GOVERNOR_ON_BAT="powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT="power";
-
-      CPU_SCALING_GOVERNOR_ON_AC="powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC="power";
-    };
-  };
-  hardware.i2c.enable = true;
-  hardware.sane.enable = true;
 
   networking.hostName = "alphabril-tuxedo-nixos"; # Define your hostname.
   # networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
